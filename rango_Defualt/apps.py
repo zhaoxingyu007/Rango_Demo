2a442b70520251170cc974dea8620d574fd6720c
yufeicheniffy <efiechan@outlook.com>
2020-02-08 16:20:02 +0800

    set default django and urls

---
+++ rango_Defualt/apps.py
@@ -0,0 +1,5 @@
+from django.apps import AppConfig
+
+
+class RangoDefualtConfig(AppConfig):
+    name = 'rango_Defualt'
---
+++ rango_Defualt/urls.py
@@ -0,0 +1,9 @@
+from django.urls import path
+from django.conf.urls import url
+from rango_Defualt import views
+
+app_name = 'rango_Defualt'
+
+urlpatterns=[
+    path('',views.homepage,name='homepage'),
+]
